b14628ea1afc7f4a1f0353afddeafcb591eec846
Ferran Llop <ferranllop@gmail.com>
2021-07-08 11:43:20 +0200

    Add example and script to run it

---
+++ examples/failingPrecondition.js
@@ -0,0 +1,8 @@
+import { ClientClass } from '../test/ClientClass.js'
+console.log('##################')
+console.log('# EXAMPLE STARTS #')
+console.log('##################')
+
+new ClientClass().offendingMethod()
+
+console.log('APPLICATION ENDS - THIS CONSOLE.LOG MUST NOT BE EXECUTED')
